daf60baaa8f16b13e1831044039d49a894979696
LucasSVA <lucas.svanadze@gmail.com>
2022-06-27 16:32:05 +0200

    ex 2  done

--- car.js
+++ car.js
@@ -1,30 +1,82 @@
-class Car {
-    constructor(brand, speed) {
+// class Car {
+//     constructor(brand, speed) {
+//         this.brand = brand
+//         this.speed = speed
+
+//     }
+
+//     accelerate = (num) => {
+//         this.speed = this.speed + num
+//     }
+
+//     brake = (num) => {
+//         this.speed = this.speed - num
+
+
+//     }
+
+//     describe() {
+//         console.log(`${this.brand} is running at ${this.speed} km/h`);
+//     }
+// }
+
+// let ford = new Car("ford", 0)
+
+
+// ford.describe()
+// ford.accelerate(50)
+// ford.describe()
+// ford.brake(25)
+// ford.describe()
+
+
+
+class tv {
+    constructor(brand) {
         this.brand = brand
-        this.speed = speed
+        this.channel = 1
+        this.volume = 50
+    }
+
+    increase = () => {
+        if (this.volume < 100 || this.volume > 0) {
+            this.volume += 1
+        }
+    }
+
+    decrease = () => {
+        if (this.volume < 100 || this.volume > 0) {
+            this.volume -= 1
+        }
 
     }
 
-    accelerate = (num) => {
-        this.speed = this.speed + num
+    channelChange = () => {
+        if (this.channel < 0 || this.channel > 50) {
+            this.channel = 1
+
+        } else {
+            this.channel += 1
+        }
+
     }
 
-    brake = (num) => {
-        this.speed = this.speed - num
-
-
+    reset = () => {
+        this.volume = 50
+        this.channel = 1
     }
+    describe = () => (
+        console.log(`Model : ${this.brand}; Volume : ${this.volume}`)
+    )
 
-    describe() {
-        console.log(`${this.brand} is running at ${this.speed} km/h`);
-    }
 }
 
-let ford = new Car("ford", 0)
+let test = new tv("Sony")
+//test.describe()
+test.increase()
+test.increase()
+test.decrease()
+test.channelChange()
+test.reset()
 
-
-ford.describe()
-ford.accelerate(50)
-ford.describe()
-ford.brake(25)
-ford.describe()
+console.log(test);
